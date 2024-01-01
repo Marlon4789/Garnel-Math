b002f0a2139f027e1ff1c146bc81055855d988f5
Marlon4789 <cuartasm844@gmail.com>
2024-01-01 14:04:13 -0500

    Nueva actualización de archivo settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,7 +85,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
