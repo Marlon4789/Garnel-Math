ada0490a4cc74fcbebd6e09662fc94b5f84164ab
Marlon4789 <cuartasm844@gmail.com>
2024-01-08 19:31:08 -0500

    Actualizando home con suscripción e iconos de redes sociales

--- garnelmath/views.py
+++ garnelmath/views.py
@@ -9,10 +9,26 @@ from .forms import SubscriptionForm, FeedbackForm
 
 # Inicio
 def home(request):
-    return render(request, 'layouts/home.html')
+    #return render(request, 'layouts/home.html')
+
+    # form subscripción
+    if request.method == 'GET':
+        return render(request, 'layouts/home.html', {'form': SubscriptionForm})
+    else:
+        try:
+            form = SubscriptionForm(request.POST)
+            new_sub = form.save(commit=False)
+            new_sub.save()
+            return render(request, 'layouts/home.html', {
+                'success': 'todo va bien.',
+            })
+        except ValueError:
+            return render(request, 'layouts/home.html', {
+                'form': SubscriptionForm,
+                'error': 'Este correo ya existe.'
+            })
 
 # Layout del panel para contenido
-
 def math_content_panel(request):    
     return render(request, 'layouts/math_content_panel.html')
 
@@ -28,5 +44,8 @@ def post_detail(request, post_id):
             new_feedback = form.save()
     else:
         form = FeedbackForm()
-        
+
     return render(request, 'content/post_detail.html', {'post':post, 'posts':posts})
+
+
+
